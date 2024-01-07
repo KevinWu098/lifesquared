d927e7f445b092ae00299a61633699f84866e838
Kevin Wu <kevinwu098@gmail.com>
2024-01-07 00:23:32 -0800

    feat: add stricter date validation

--- src/components/CreationForm.tsx
+++ src/components/CreationForm.tsx
@@ -26,10 +26,39 @@ import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast";
 import { trpc } from "@/app/_trpc/client";
 
+import LoginComponent from "./LoginComponent";
+
 const birthdayRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+
+const isDateValid = (dateStr: string) => {
+    if (!dateStr.match(birthdayRegex)) {
+        return false;
+    }
+
+    const parts = dateStr.split("/");
+    const month = parseInt(parts[0], 10);
+    const day = parseInt(parts[1], 10);
+    const year = parseInt(parts[2], 10);
+
+    if (isNaN(month) || isNaN(day) || isNaN(year)) {
+        return false;
+    }
+
+    const dateObject = new Date(year, month - 1, day);
+    if (
+        dateObject.getMonth() !== month - 1 ||
+        dateObject.getDate() !== day ||
+        dateObject.getFullYear() !== year
+    ) {
+        return false;
+    }
+
+    return true;
+};
+
 const FormSchema = z.object({
-    birthday: z.string().regex(birthdayRegex, {
-        message: "Invalid birthday format. Please use mm/dd/yyyy.",
+    birthday: z.string().refine((data) => isDateValid(data), {
+        message: "Invalid birthday format or date. Please use mm/dd/yyyy.",
     }),
     finalYear: z.string().refine(
         (data) => {
